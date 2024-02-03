2e7887d703a46e80e703daa97c2f5b78e12604d3
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-02-03 10:27:20 +0100

    exporting everything and removed test code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-type DatabaseTable = {
+export type DatabaseTable = {
   [key: string]: string | number | boolean
 }
 
-type DatabaseSchema = {
+export type DatabaseSchema = {
   [key: string]: DatabaseTable
 }
 
-type InitialDatabaseData<GenericDatabaseSchema extends DatabaseSchema> = {
+export type InitialDatabaseData<GenericDatabaseSchema extends DatabaseSchema> = {
   [TableName in keyof GenericDatabaseSchema]: Array<{
     [ColumnName in keyof GenericDatabaseSchema[TableName]]: GenericDatabaseSchema[TableName][ColumnName]
   }>
 }
 
-const createDatabase = <GenericDatabaseSchema extends DatabaseSchema>(initialData: InitialDatabaseData<GenericDatabaseSchema>) => {
+export const createDatabase = <GenericDatabaseSchema extends DatabaseSchema>(initialData: InitialDatabaseData<GenericDatabaseSchema>) => {
   return {
     all: <Table extends keyof GenericDatabaseSchema>({ table }: { table: Table }): Array<GenericDatabaseSchema[Table]> => {
       return initialData[table] as Array<GenericDatabaseSchema[Table]>;
@@ -57,70 +57,3 @@ const createDatabase = <GenericDatabaseSchema extends DatabaseSchema>(initialDat
     },
   };
 };
-
-type Database = {
-  users: {
-    firstname: string,
-    language?: string
-  },
-  motorcycles: {
-    brand: string,
-    model: string
-  }
-}
-
-const { all, one, add, remove, update } = createDatabase<Database>({
-  users: [],
-  motorcycles: []
-});
-
-add({
-  table: "users",
-  data: {
-    firstname: "Amin",
-  }
-});
-
-add({
-  table: "users",
-  data: {
-    firstname: "Amin",
-  }
-});
-
-add({
-  table: "users",
-  data: {
-    firstname: "Anas",
-  }
-});
-
-remove({
-  table: "users",
-  criterias: {
-    firstname: "Amin"
-  }
-});
-
-update({
-  table: "users",
-  criterias: {
-    firstname: "Anas"
-  },
-  data: {
-    firstname: "Nanouss"
-  }
-});
-
-const user = one({
-  table: "users",
-  criterias: {
-    firstname: "Nanouss"
-  }
-});
-
-const users = all({ table: "users" });
-const motorcycles = all({ table: "motorcycles" });
-
-console.log(users);
-console.log(user);
