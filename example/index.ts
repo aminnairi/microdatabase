f10d14f7b6673628dde97cecba7270ac53693907
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-02-04 10:34:48 +0100

    added an example

---
+++ example/index.ts
@@ -0,0 +1,33 @@
+import { InMemoryPersistence, createDatabase } from "../src";
+import { randomUUID } from "crypto";
+
+type Database = {
+  users: {
+    id: string,
+    email: string,
+    username: string
+  }
+}
+
+const { persist, all, add } = await createDatabase<Database>({
+  persistence: new InMemoryPersistence<Database>({
+    users: []
+  })
+});
+
+add({
+  table: "users",
+  data: {
+    id: randomUUID(),
+    email: "email@domain.com",
+    username: "user123"
+  }
+});
+
+const users = all({
+  table: "users"
+});
+
+console.log(users);
+
+await persist();
